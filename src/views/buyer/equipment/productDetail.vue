b1274a19ab18974a2f63e28e6cc71f879cf351fd
maoyaY <1335679005@qq.com>
2018-10-31 15:21:47 +0800

    规格选择

--- src/views/buyer/equipment/productDetail.vue
+++ src/views/buyer/equipment/productDetail.vue
@@ -25,7 +25,7 @@
           <span>快递：0.00</span>
         </div>
       </div>
-      <div class="m-product-detail-more">
+      <div class="m-product-detail-more" @click="changeModal('show_sku',true)">
         <div>
           <span class="m-label">规格</span>
           <span class="m-select m-black">选择    颜色分类 重量</span>
@@ -49,7 +49,6 @@
         </div>
       </div>
       <div class="m-product-detail-img-box">
-
         <img src=""  alt="">
       </div>
       <div class="m-product-detail-foot">
@@ -60,17 +59,93 @@
           <span class="active">立即购物车</span>
         </div>
       </div>
+
+      <div class="m-sku" v-if="show_sku">
+        <div class="m-sku-state">
+          <span class="m-icon-close" @click="changeModal('show_sku',false)"></span>
+          <div class="m-sku-content">
+             <div class="m-sku-img-box m-center">
+               <img src=""  alt="">
+             </div>
+            <div  class="m-center">
+              <span class="m-red">￥193.0</span>
+            </div>
+            <div class="m-scroll">
+              <ul class="m-sku-box">
+                <li>
+                  <p>颜色分类</p>
+                  <ul class="m-sku-ul">
+                    <li>红色</li>
+                    <li>红色</li>
+                    <li>红色</li>
+                    <li class="active">红色</li>
+                  </ul>
+                </li>
+                <li>
+                  <p>颜色分类</p>
+                  <ul class="m-sku-ul">
+                    <li>红色</li>
+                    <li>红色</li>
+                    <li class="active">红色</li>
+                  </ul>
+                </li>
+              </ul>
+              <div class="m-sku-num">
+                <span>购买数量</span>
+                <div class="m-num">
+                  <span class="m-icon-cut"></span>
+                  <input type="text" class="m-num-input" placeholder="0">
+                  <span class="m-icon-add"></span>
+                </div>
+              </div>
+            </div>
+            <div class="m-sku-btn">
+              <span>确 定</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
+  var scroll = (function (className) {
+    var scrollTop;
+    return {
+      afterOpen: function () {
+        scrollTop = document.scrollingElement.scrollTop || document.body.scrollTop;
+        document.body.classList.add(className);
+        document.body.style.top = -scrollTop + 'px';
+      },
+      beforeClose: function () {
+        document.body.classList.remove(className);
+        document.scrollingElement.scrollTop = scrollTop;
+        document.body.scrollTop = scrollTop;
+      }
+    };
+  })('scroll');
     export default {
-        name: "product-detail"
+        data(){
+          return{
+            show_sku:false
+          }
+        },
+      methods:{
+         changeModal(v,bool) {
+           this[v] = bool;
+           if(bool){
+             scroll.afterOpen();
+           }else{
+             scroll.beforeClose();
+           }
+         }
+      }
     }
 </script>
 
 <style lang="less" rel="stylesheet/less" scoped>
 .m-product-detail{
+  padding-bottom: 200px;
   .m-product-swipe{
     position: relative;
     width: 100%;
@@ -210,5 +285,131 @@
       }
     }
   }
+  .m-sku{
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    /*background-color: rgba(0,0,0,0.2);*/
+    z-index: 1001;
+    transition: opacity .5s;
+    .m-sku-state{
+      background-color: #fff;
+      position: absolute;
+      width: 100%;
+      height: 990px;
+      left: 0;
+      bottom:0;
+      border-radius:50px 50px 0 0;
+      -webkit-transition: height 0.88s;
+      transition: height 0.88s;
+      .m-icon-close{
+        position: absolute;
+        top: 35px;
+        right: 35px;
+        height: 35px;
+        width: 35px;
+        background: url("/static/images/icon-close.png") no-repeat;
+        background-size: 100% 100%;
+      }
+      .m-sku-content{
+        .m-center{
+          text-align: center;
+          .m-red{
+            color: #EF9B2D;
+            font-size: 30px;
+            font-weight:400;
+          }
+        }
+        .m-sku-img-box{
+          margin: 30px 0 10px;
+          img{
+            display: inline-block;
+            width: 230px;
+            height: 230px;
+            box-shadow:0px 5px 6px rgba(0,0,0,0.16);
+          }
+        }
+        .m-scroll{
+          height: 450px;
+          overflow-y: auto;
+          overflow-x: hidden;
+          padding: 10px 65px;
+          color: #999999;
+          .m-sku-box{
+            text-align: left;
+            list-style: none;
+            padding: 0;
+            .m-sku-ul{
+              display: flex;
+              flex-flow: row;
+              align-items: center;
+              justify-content: flex-start;
+              flex-wrap: wrap;
+              list-style: none;
+              padding: 0;
+              overflow-y: scroll;
+              li{
+                padding: 5px 56px;
+                background-color: #D5D5D5;
+                border-radius: 30px;
+                margin-right: 30px;
+                margin-bottom: 20px;
+                &.active{
+                  background-color: #FCD316;
+                  color: #333333;
+                }
+              }
+            }
+          }
+          .m-sku-num{
+            display: flex;
+            flex-flow: row;
+            align-items: center;
+            justify-content: space-between;
+            width: 100%;
+            .m-icon-add{
+              display: inline-block;
+              width: 42px;
+              height: 42px;
+              background: url("/static/images/icon-add.png") no-repeat;
+              background-size: 100% 100%;
+              vertical-align: middle;
+            }
+            .m-icon-cut{
+              display: inline-block;
+              width: 42px;
+              height: 42px;
+              background: url("/static/images/icon-cut.png") no-repeat;
+              background-size: 100% 100%;
+              vertical-align: middle;
+            }
+            .m-num-input{
+              display: inline-block;
+              width: 87px;
+              padding: 5px  0;
+              border: none;
+              text-align: center;
+            }
+          }
+        }
+        .m-sku-btn{
+          margin-top: 53px;
+          span{
+            display: inline-block;
+            width: 680px;
+            background-color: #fcd316;
+            height: 62px;
+            line-height: 62px;
+            font-size: 30px;
+            border-radius: 10px;
+          }
+        }
+      }
+    }
+
+  }
 }
+
 </style>
