93f4fbb85bff996ea9fbfc2b0ba0fce7d8b07e7d
Liuxy <liuxy0551@qq.com>
2018-11-29 17:02:58 +0800

    样式微调

--- src/views/buyer/shop/index.vue
+++ src/views/buyer/shop/index.vue
@@ -372,109 +372,111 @@
 </script>
 <style lang="less" rel="stylesheet/less" scoped>
   @import "../../../common/css/index";
-.m-shop{
-  width: 100%;
-  min-height: 100%;
-  background-color: #EEEEEE;
-  position: relative;
-  /*z-index: -2;*/
-  .m-shop-bg{
+
+  .m-shop{
     width: 100%;
-    height: 450px;
-    background:linear-gradient(180deg,rgba(252,211,22,1) 0%,rgba(239,232,197,1) 100%);
-    position: absolute;
-    top:0;
-    left: 0;
-    /*z-index: -1;*/
-    .m-icon-bg{
-      position: absolute;
-      bottom: -3px;
-      left: 0;
+    min-height: 100%;
+    background-color: #ffffff;
+    position: relative;
+    /*z-index: -2;*/
+    .m-shop-bg{
       width: 100%;
-      height: 67px;
-      background: url("/static/images/icon-bg.png") no-repeat;
-      background-size: 100% 100%;
-    }
-  }
-  .m-shop-content{
-    padding: 0 25px 200px 25px;
-    text-align: left;
-    position: absolute;
-    top:0;
-    left: 0;
-    .m-no-coupon{
-      margin: 200px 80px 0 100px;
-    }
-    .m-shop-title{
-      .flex-row(space-between);
-      padding: 20px 0;
-      .m-title{
-        font-size: 38px;
-        font-weight: bold;
-      }
-    }
-    .m-p{
-      padding: 0 0 40px;
-    }
-    .m-shop-one{
-      background-color: #fff;
-      width: 654px;
-      padding: 23px;
-      border-radius: 10px;
-      box-shadow:0 5px 6px rgba(0,0,0,0.16);
-      color: #333;
-      margin-bottom: 20px;
-      .m-icon-radio{
-        margin-right: 35px;
-      }
-      .m-icon-more{
-        display: inline-block;
-        width: 23px;
-        height: 23px;
-        background: url("/static/images/icon-more.png") no-repeat;
+      height: 450px;
+      background:linear-gradient(180deg,rgba(252,211,22,1) 0%,rgba(239,232,197,1) 100%);
+      position: absolute;
+      top:0;
+      left: 0;
+      /*z-index: -1;*/
+      .m-icon-bg{
+        position: absolute;
+        bottom: -3px;
+        left: 0;
+        width: 100%;
+        height: 67px;
+        background: url("/static/images/icon-bg.png") no-repeat;
         background-size: 100% 100%;
-        margin-left: 25px;
-        vertical-align: text-top;
       }
-      .m-shop-store-name{
-        font-weight: 400;
+    }
+    .m-shop-content{
+      padding: 0 25px 200px 25px;
+      text-align: left;
+      position: absolute;
+      top:0;
+      left: 0;
+      .m-no-coupon{
+        margin: 200px 80px 0 100px;
       }
-      .m-shop-product{
-        display: flex;
-        flex-flow: row;
-        align-items: center;
-        justify-content: flex-start;
-        margin: 30px 0;
-        .m-product-info{
+      .m-shop-title{
+        .flex-row(space-between);
+        padding: 20px 0;
+        .m-title{
+          font-size: 38px;
+          font-weight: bold;
+        }
+      }
+      .m-p{
+        padding: 0 0 40px;
+      }
+      .m-shop-one{
+        background-color: #fff;
+        width: 654px;
+        padding: 23px;
+        border-radius: 10px;
+        box-shadow:0 5px 6px rgba(0,0,0,0.16);
+        color: #333;
+        margin-bottom: 20px;
+        .m-icon-radio{
+          margin-right: 35px;
+        }
+        .m-icon-more{
+          display: inline-block;
+          width: 23px;
+          height: 23px;
+          background: url("/static/images/icon-more.png") no-repeat;
+          background-size: 100% 100%;
+          margin-left: 25px;
+          vertical-align: text-top;
+        }
+        .m-shop-store-name{
+          font-weight: 400;
+        }
+        .m-shop-product{
           display: flex;
           flex-flow: row;
-          align-items: flex-start;
+          align-items: center;
           justify-content: flex-start;
-          .m-product-img{
-            display: block ;
-            width: 153px;
-            height: 153px;
-            background-color: #edb3b1;
-            margin-right: 30px;
-          }
-          .m-text-info{
-            width: 410px;
-            h3{
-              font-weight: 400;
+          margin: 30px 0;
+          .m-product-info{
+            display: flex;
+            flex-flow: row;
+            align-items: flex-start;
+            justify-content: flex-start;
+            .m-product-img{
+              display: block ;
+              width: 153px;
+              height: 153px;
+              background-color: #edb3b1;
+              margin-right: 30px;
             }
-            .m-product-sku-select-p{
-              margin: 30px 0 30px;
-              .m-product-sku-select{
-                padding: 8px 20px;
-                background-color: #E9E9E9;
-                border-radius: 10px;
-                .m-sku-more{
-                  display: inline-block;
-                  width: 16px;
-                  height: 16px;
-                  background: url("/static/images/icon-down.png") no-repeat;
-                  background-size: 100% 100%;
-                  margin-left: 23px;
+            .m-text-info{
+              width: 410px;
+              h3{
+                font-weight: 400;
+              }
+              .m-product-sku-select-p{
+                margin: 30px 0 30px;
+                .m-product-sku-select{
+                  padding: 8px 20px;
+                  background-color: #E9E9E9;
+                  border-radius: 10px;
+                  .m-sku-more{
+                    display: inline-block;
+                    width: 16px;
+                    height: 16px;
+                    background: url("/static/images/icon-down.png") no-repeat;
+                    background-size: 100% 100%;
+                    margin-left: 23px;
+                  }
                 }
               }
             }
@@ -482,48 +484,48 @@
         }
       }
     }
-  }
-  .m-shop-foot{
-    position: fixed;
-    bottom: 103px;
-    left: 0;
-    height: 90px;
-    padding: 0 50px;
-    width: 650px;
-    background-color: #fff;
-    z-index: 10;
-    .flex-row(space-between);
-    color: #999999;
-    line-height: 60px;
-    .m-red{
-      color: @mainColor;
-      font-size: 30px;
-      margin: 0 10px;
+    .m-shop-foot{
+      position: fixed;
+      bottom: 103px;
+      left: 0;
+      height: 90px;
+      padding: 0 50px;
+      width: 650px;
+      background-color: #fff;
+      z-index: 10;
+      .flex-row(space-between);
+      color: #999999;
+      line-height: 60px;
+      .m-red{
+        color: @mainColor;
+        font-size: 30px;
+        margin: 0 10px;
+      }
+      .m-shop-btn{
+        display: inline-block;
+        width: 193px;
+        height: 60px;
+        line-height: 70px;
+        background:linear-gradient(90deg,rgba(245,236,178,1) 0%,rgba(252,211,22,1) 100%);
+        border-radius: 30px;
+        font-size: 30px;
+        color: #333;
+      }
     }
-    .m-shop-btn{
+    .m-icon-radio{
       display: inline-block;
-      width: 193px;
-      height: 60px;
-      background:linear-gradient(90deg,rgba(245,236,178,1) 0%,rgba(252,211,22,1) 100%);
-      border-radius: 30px;
-      font-size: 30px;
-      color: #333;
-    }
-  }
-  .m-icon-radio{
-    display: inline-block;
-    width: 30px;
-    height: 32px;
-    background: url("/static/images/icon-radio.png") no-repeat;
-    background-size: 100% 100%;
-    vertical-align: text-top;
-    &.active{
-      background: url("/static/images/icon-radio-active.png") no-repeat;
+      width: 30px;
+      height: 32px;
+      background: url("/static/images/icon-radio.png") no-repeat;
       background-size: 100% 100%;
+      vertical-align: text-top;
+      &.active{
+        background: url("/static/images/icon-radio-active.png") no-repeat;
+        background-size: 100% 100%;
+      }
+    }
+    .m-radio-margin {
+      margin-top: 65px;
     }
   }
-  .m-radio-margin {
-    margin-top: 65px;
-  }
-}
 </style>
