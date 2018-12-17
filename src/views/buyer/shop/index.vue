50bed54e1f5c4b8f62fb07062911c4d3e6b34c51
Liuxy <liuxy0551@qq.com>
2018-12-17 20:19:12 +0800

    样式微调

--- src/views/buyer/shop/index.vue
+++ src/views/buyer/shop/index.vue
@@ -398,7 +398,7 @@
         left: 0;
         width: 100%;
         height: 67px;
-        background: url("/static/images/icon-bg-grey.png") no-repeat;
+        background: url("/static/images/icon-bg.png") no-repeat;
         background-size: 100% 100%;
       }
     }
