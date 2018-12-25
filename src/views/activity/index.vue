d328e205285b4e156f64deee7914d8ecec2f96ab
Liuxy <liuxy0551@qq.com>
2018-12-25 15:44:46 +0800

    样式优化

--- src/views/activity/index.vue
+++ src/views/activity/index.vue
@@ -9,7 +9,7 @@
       <el-table-column label="活动名称" align="center" prop="acname"></el-table-column>
       <el-table-column label="类别" align="center" prop="actype_zh"></el-table-column>
       <el-table-column label="商品数" align="center" prop="pblogo"></el-table-column>
-      <el-table-column label="开启/关闭" align="center" prop="pblogo">
+      <el-table-column label="关闭/开启" align="center" prop="pblogo">
         <template slot-scope="scope">
           <el-switch v-model="scope.row.acshow" @change="showActivity(scope.row)" active-color="#409EFF" inactive-color="#DBDCDC">
           </el-switch>
