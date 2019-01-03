6bdc705ed41112211a4567e654ea73e328820fc7
Liuxy <liuxy0551@qq.com>
2019-01-03 22:01:39 +0800

    签到设置

--- src/views/marketing/attendance.vue
+++ src/views/marketing/attendance.vue
@@ -2,21 +2,20 @@
   <div class="container">
     <el-row>
       <el-col :span="16">
-        <block-title title="基本信息"></block-title>
-        <el-form :model="signInForm" ref="signInForm" label-position="left" label-width="100px">
-          <el-form-item v-for="(rule, index) in signInForm.rules" :label="'规则' + (index + 1)" :key="rule.key"
-          :prop="'rules.' + index + '.value'" :rules="{
-          required: true, message: ''}" inline>
+        <block-title title="签到设置"></block-title>
+        <el-form label-position="left" label-width="100px">
+          <el-form-item v-for="(rule, index) in rules" :label="'规则 ' + (index + 1)" :key="index">
             <span>连续签到</span>
-            <el-input class="item-input" v-model="rule.value"></el-input>
+            <el-input class="item-input" v-model="rule.day"></el-input>
             <span>天后，每日获得</span>
-            <el-input class="item-input"></el-input>
+            <el-input class="item-input" v-model="rule.num"></el-input>
             <span>积分</span>
-            <el-button style="margin-left: 50px" @click.prevent="removeRule(rule)">删除</el-button>
+            <el-button style="margin-left: 50px" v-if="index > 0" @click="removeRule(index)">删除</el-button>
           </el-form-item>
-          <el-form-item style="margin-left: 73px">
+          <el-form-item class="btn-box">
             <el-button @click="addRule">添加规则</el-button>
-            <el-button type="primary" @click="checkFormData">保存资讯</el-button>
+            <el-button @click="getRule">取 消</el-button>
+            <el-button type="primary" @click="saveSetting">保存设置</el-button>
           </el-form-item>
         </el-form>
       </el-col>
@@ -29,47 +28,30 @@
     name: 'Attendance',
     data() {
       return {
-        signInForm: {
-          rules: [{ day: '', num: '' }]
-        },
+        rules: [
+          { day: 1, num: 5 }, { day: 5, num: 10 }, { day: 10, num: 15 }, { day: 20, num: 20 }
+        ]
       }
     },
     components: {},
     methods: {
+      // 获取设置
+      getRule() {
+
+      },
+      // 添加一行
       addRule() {
-        this.signInForm.rules.push({
-          day: '',
-          num: '',
-          key: Date.now()
-        })
+        let rule = { day: '', num: '' };
+        this.rules.push(rule)
       },
       // 移除一行
-      removeRule(rule) {
-        let index = this.signInForm.rules.indexOf(rule);
-        if(index !== -1) {
-          this.signInForm.rules.splice(index, 1)
-        }
+      removeRule(index) {
+        this.rules.splice(index, 1)
       },
-      checkFormData() {
-        this.$refs.circleFormRef.validate(valid => {
-          if (valid) {
-            let title = '新增';
-            this.$http.post(this.$api.create_news, this.circleForm).then(res => {
-              if (res.data.status == 200) {
-                this.initCircleForm();
-                this.$router.push('/circle/circle');
-                this.$notify({
-                  title: `${title}成功`,
-                  message: `资讯标题：${this.circleForm.netitle}成功`,
-                  type: 'success'
-                });
-              }
-            });
-          }else {
-            this.$message.warning('请根据校验信息完善表单!');
-          }
-        })
-      },
+      // 保存设置
+      saveSetting() {
+        console.log(this.rules);
+      }
     }
   }
 </script>
@@ -83,6 +65,12 @@
     }
     span {
       margin: 0 0.4rem;
+    }
+    .btn-box {
+      margin: 43px;
+      .el-button {
+        margin-left: 30px;
+      }
     }
   }
 </style>
