0ddbafef98aa31cbd8facf1f95bd5db088b09edd
maoyaY <1335679005@qq.com>
2019-03-07 09:17:46 +0800

    活动页面

--- src/views/buyer/activity/index.vue
+++ src/views/buyer/activity/index.vue
@@ -80,14 +80,15 @@
     min-height: 100%;
     background-color: #ffffff;
     margin-bottom: -100px;
+    padding-top: 20px;
     .m-activity-top-bg {
       width: 750px;
       height: 350px;
       background: linear-gradient(180deg, @mainColor 0%, @subColor 100%);
     }
     .m-activity-box {
-      position: absolute;
-      top: 20px;
+      /*position: absolute;*/
+      /*top: 20px;*/
       .m-activity-item {
         width: 700px;
         height: 500px;
