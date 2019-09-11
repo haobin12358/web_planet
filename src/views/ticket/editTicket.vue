73c7ecd41638409fcbbc5ff73d46a05de33d073a
maoyaY <17774012962@163.com>
2019-09-11 10:39:08 +0800

    门票修改

--- src/views/ticket/editTicket.vue
+++ src/views/ticket/editTicket.vue
@@ -24,6 +24,28 @@
             </div>
           </el-upload>
         </el-form-item>
+          <el-form-item label="顶部轮播图" prop="tibanner">
+            <div class="m-up-img-box">
+              <div class="inputbg m-img-xl el-upload-list--picture-card" v-for="(item,index) in formData.tibanner">
+                <img :src="item"  style="width: 288px;height:161px;"/>
+                <span class="el-upload-list__item-actions">
+                <span class="el-upload-list__item-preview" @click="previewImage(item)">
+                  <i class="el-icon-zoom-in"></i>
+                </span>
+                <span class="el-upload-list__item-delete" @click="deleteImage(index)">
+                  <i class="el-icon-delete"></i>
+                </span>
+              </span>
+              </div>
+              <div class="inputbg m-img-xl"><span>+</span><input type="file" multiple="multiple" id="prDesc"  accept="image/*" @change="newUploadImages"></div>
+            </div>
+              <div slot="tip" class="el-upload__tip">
+                <span>可多选,建议尺寸为750x375(px),大小不要超过15M,上传成功后会显示,上传大图请耐心等待.</span>
+              </div>
+
+
+
+          </el-form-item>
           <el-form-item label="发放时间" required>
             <el-date-picker
               v-model="time"
@@ -60,10 +82,10 @@
               <template slot="append">张</template>
             </el-input>
           </el-form-item>
-          <el-form-item label="规则" prop="tirules">
-            <el-input v-model="formData.tirules" type="textarea">
-            </el-input>
-          </el-form-item>
+<!--          <el-form-item label="规则" prop="tirules">-->
+<!--            <el-input v-model="formData.tirules" type="textarea">-->
+<!--            </el-input>-->
+<!--          </el-form-item>-->
           <el-form-item label="授权资质" >
             <el-upload
               class="avatar-uploader m-draft"
@@ -93,25 +115,25 @@
               </el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="短语简介" prop="tiabbreviation">
-            <el-input v-model="formData.tiabbreviation" placeholder="最好在10字以内"></el-input>
-          </el-form-item>
-          <el-form-item label="标签" prop="sspcontent">
-            <div class="m-label-box">
-              <span class=" m-label" :class="item.active ? 'active':''" v-for="(item,index) in enter_list" @click="labelClick(index,'enter_list')">{{item.name}}</span>
-              <el-popover
-                placement="right"
-                width="400"
-                trigger="manual"
-                v-model="enterVisible">
-                <h3 class="el-upload__tip">新增报名填写项</h3>
-                <el-input v-model="label" maxlength="4" placeholder="5字以内" class="m-input-label"></el-input>
-                <el-button type="primary" @click="enterSave">保 存</el-button>
-                <el-button @click="enterVisible = false">取消</el-button>
-                <span class="m-label" slot="reference" @click="enterVisible =!enterVisible">+</span>
-              </el-popover>
-            </div>
-          </el-form-item>
+<!--          <el-form-item label="短语简介" prop="tiabbreviation">-->
+<!--            <el-input v-model="formData.tiabbreviation" placeholder="最好在10字以内"></el-input>-->
+<!--          </el-form-item>-->
+<!--          <el-form-item label="标签" prop="sspcontent">-->
+<!--            <div class="m-label-box">-->
+<!--              <span class=" m-label" :class="item.active ? 'active':''" v-for="(item,index) in enter_list" @click="labelClick(index,'enter_list')">{{item.name}}</span>-->
+<!--              <el-popover-->
+<!--                placement="right"-->
+<!--                width="400"-->
+<!--                trigger="manual"-->
+<!--                v-model="enterVisible">-->
+<!--                <h3 class="el-upload__tip">新增报名填写项</h3>-->
+<!--                <el-input v-model="label" maxlength="4" placeholder="5字以内" class="m-input-label"></el-input>-->
+<!--                <el-button type="primary" @click="enterSave">保 存</el-button>-->
+<!--                <el-button @click="enterVisible = false">取消</el-button>-->
+<!--                <span class="m-label" slot="reference" @click="enterVisible =!enterVisible">+</span>-->
+<!--              </el-popover>-->
+<!--            </div>-->
+<!--          </el-form-item>-->
 <!--          <el-form-item label="开启/中止" >-->
 <!--            <el-switch-->
 <!--              v-model="formData.interrupt"-->
@@ -126,11 +148,16 @@
       </el-col>
     </el-row>
     <input type="file" multiple="true" id="quill-img" class="quill-img" accept="image/*" @change="fileChange">
+    <!--预览大图dialog-->
+    <el-dialog :visible.sync="dialogVisible" top="8vh">
+      <img width="100%" :src="dialogImageUrl" alt="">
+    </el-dialog>
   </div>
 </template>
 
 <script>
   import { quillEditor } from "vue-quill-editor";
+  import ImgsDragSort from 'src/components/ImgsDragSort/index'
   import 'quill/dist/quill.js';
   import { getStore } from "src/utils/index";
   const toolbarOptions = [
@@ -164,7 +191,8 @@
           liids:[],
           delete:false,
           titripstarttime:'',
-          titripendtime:''
+          titripendtime:'',
+          tibanner:[]
         },
         options: [],
         rules: {
@@ -195,6 +223,9 @@
           tiabbreviation: [
             { required: true, message: '短语简介必填', trigger: 'blur' }
           ],
+          tibanner:[
+            { required: true, message: '票务轮播图必填', trigger: 'change' }
+          ]
         },
         time:[],
         time1:[],
@@ -217,7 +248,9 @@
             }
           }
         },
-        agree:false
+        agree:false,
+        dialogImageUrl:'',
+        dialogVisible:false
       }
     },
     computed: {
@@ -235,7 +268,7 @@
 
       },
     },
-    components: {quillEditor},
+    components: {quillEditor,ImgsDragSort},
     mounted() {
       this.getLink();
       if(this.$route.query.id){
@@ -251,8 +284,40 @@
         this.formData.tiimg = res.data;
         console.log(this.formData.tiimg)
       },
+
       handleCertPicSuccess(res,file){
         this.formData.ticertificate = res.data;
+      },
+      newUploadImages(e){
+
+        for(let i =0;i<e.target.files.length;i++){
+          let formData = new FormData();
+          formData.append('file', e.target.files[i]);
+          this.$http({method: 'post', url: this.uploadUrl, data: formData,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}}).then(
+            res => {
+              if (res.data.status == 200) {
+                // this.imagesUrl.push({
+                //   name: file.file.name,
+                //   url: res.data.data
+                // })
+                let file = document.getElementById('prDesc');
+                file.value ='';
+
+                this.formData.tibanner.push(res.data.data);
+              }else{
+                this.$message.error(res.data.message);
+              }
+            }
+          )
+        }
+      },
+      previewImage(item){
+        this.dialogImageUrl = item;
+        this.dialogVisible = true;
+      },
+      deleteImage(index){
+        this.formData.tibanner.splice(index,1)
       },
       // 上传前限制小于15M
       beforeImgUpload(file) {
@@ -262,6 +327,7 @@
         }
         return isLt15M;
       },
+
       getLink(){
         this.$http.get(this.$api.list_linkage, {
           noLoading: true,
@@ -308,12 +374,13 @@
                   ticertificate:data.ticertificate,
                   tideposit:data.tideposit,
                   tiabbreviation:data.tiabbreviation,
-                  ticategory:data.ticategory,
+                  // ticategory:data.ticategory,
                   liids:[],
                   delete:false,
                   tiid:data.tiid,
                 titripendtime:data.titripendtime,
-                titripstarttime:data.titripstarttime
+                titripstarttime:data.titripstarttime,
+                tibanner:data.tibanner
               }
               // this.time[0] = new Date(this.formData.plstarttime);
               // this.time[1] = new Date(this.formData.plendtime);
@@ -328,14 +395,15 @@
               }
               this.time = [this.formData.tistarttime, this.formData.tiendtime];
               this.time1 = [this.formData.titripstarttime,this.formData.titripendtime];
+              //标签
 
-              let enter =[];
-              for(let i in this.formData.ticategory){
-                enter.push({name:this.formData.ticategory[i],active:true});
-
-              }
-
-              this.enter_list = enter;
+              // let enter =[];
+              // for(let i in this.formData.ticategory){
+              //   enter.push({name:this.formData.ticategory[i],active:true});
+              //
+              // }
+              //
+              // this.enter_list = enter;
 
             }
           }
@@ -360,15 +428,15 @@
 
         this.$refs.formData.validate(valid => {
           if (valid) {
-            let enter =[];
-
-            for(let i in this.enter_list){
-              if(this.enter_list[i].active){
-                enter.push(this.enter_list[i].name);
-              }
-            }
-
-            this.formData.ticategory = [].concat(enter);
+            // let enter =[];
+            //
+            // for(let i in this.enter_list){
+            //   if(this.enter_list[i].active){
+            //     enter.push(this.enter_list[i].name);
+            //   }
+            // }
+            console.log(this.formData.tibanner)
+            // this.formData.ticategory = [].concat(enter);
             this.formData.tistarttime = this.time[0];
             this.formData.tiendtime = this.time[1];
             this.formData.titripstarttime = this.time1[0];
@@ -441,7 +509,8 @@
         quill.insertEmbed(length,'image',res.data.data);
         quill.setSelection(length+1);
 
-      }
+      },
+
 
     }
   }
@@ -529,6 +598,67 @@
       opacity: 0;
       position: fixed;
       bottom: -100px;
+    }
+    .el-upload-list--picture-card .el-upload-list__item-actions:hover {
+      opacity: 1;
+    }
+    .m-up-img-box{
+      display: flex;
+      flex-flow: row;
+      align-items: center;
+      justify-content: flex-start;
+      flex-wrap: wrap!important;
+      margin-bottom: 20px;
+      .el-upload-list__item-actions {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+        cursor: default;
+        text-align: center;
+        color: #fff;
+        opacity: 0;
+        font-size: 20px;
+        background-color: rgba(0,0,0,.5);
+        -webkit-transition: opacity .3s;
+        transition: opacity .3s;
+        border-radius: 6px;
+        display: flex;
+        flex-flow: row;
+        align-items: center;
+        justify-content: center;
+        span {
+          cursor: pointer;
+        }
+      }
+    }
+    .inputbg{
+      margin: 0  10px  10px 0;
+      /*color: #97ADCB;*/
+      color: #999;
+      border: 1px solid #eeeeee;
+      background-color: #fbfdff;
+      border-radius: 6px;
+      font-size: 40px;
+      -webkit-box-sizing: border-box;
+      box-sizing: border-box;
+      position: relative;
+      width: 288px;
+      height: 161px;
+      line-height: 161px;
+      text-align: center;
+    }
+    .inputbg input{
+      position: absolute;
+      top: 0;
+      left: 0;
+      opacity:0;
+      filter:alpha(opacity=0);
+      width: 288px;
+      height: 161px;
+      line-height: 161px;
+      cursor: pointer;
     }
   }
 </style>
